ed742e11b536b8abd2f4affe54cb436b65c11f61
Jiacheng Yang <39355540+haijunme@users.noreply.github.com>
2022-07-04 23:09:47 -0400

    Upload the solution for "Moving Average From Data Stream" 2022-07-04 11:09pm

--- bin/queue/MovingAverageFromDataStream.rb
+++ bin/queue/MovingAverageFromDataStream.rb
@@ -7,8 +7,9 @@ class MovingAverage
     :type size: Integer
 =end
   def initialize(size)
+    @size = size
     @running_sum = 0
-    @queue = Containers::RubyDeque
+    @queue = Containers::RubyDeque.new
   end
 
 
@@ -17,8 +18,20 @@ class MovingAverage
     :rtype: Float
 =end
   def next(val)
+    if @queue.size == @size
+      @running_sum -= @queue.pop_front
+    end
+    @queue.push_back val
+    @running_sum += val
 
+    @running_sum.to_f / @queue.size
   end
 
 
 end
+
+movingAverage = MovingAverage.new 3
+puts movingAverage.next(1)
+puts movingAverage.next(10)
+puts movingAverage.next(3)
+puts movingAverage.next(5)
