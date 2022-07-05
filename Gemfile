ec53d496a5cb9f2dde06cdfa97147c5573d0b625
Jiacheng Yang <39355540+haijunme@users.noreply.github.com>
2022-07-04 22:52:12 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# The data structure library that is also used by Leetcode
+gem 'algorithms', '~> 1.0'
+
+# gem "rails"
---
+++ bin/queue/MovingAverageFromDataStream.rb
@@ -0,0 +1,24 @@
+require 'rubygems'
+require 'algorithms'
+
+class MovingAverage
+
+=begin
+    :type size: Integer
+=end
+  def initialize(size)
+    @running_sum = 0
+    @queue = Containers::RubyDeque
+  end
+
+
+=begin
+    :type val: Integer
+    :rtype: Float
+=end
+  def next(val)
+
+  end
+
+
+end
